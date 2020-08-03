4e6d1508c6cc084f5447be4d21c4f15084bb76ca
Sofia Cheung <sofiavictoria17@gmail.com>
2020-08-03 12:45:44 -0700

    Send a json object in every failed request

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -14,20 +14,20 @@ async function postNew(req, res) {
   const { email } = req.body; // const email = req.body.email;
   const pwd = req.body.password;
 
-  if (!email) res.status(400).send('Missing email');
-  if (!pwd) res.status(400).send('Missing password ');
+  if (!email) res.status(400).json({ error: 'Missing email' });
+  if (!pwd) res.status(400).json({ error: 'Missing password ' });
 
   // check if email already exists in db
   const found = await dbClient.client.collection('users').find({ email }).count();
   if (found > 0) {
-    res.status(400).send('Already exist');
+    res.status(400).json({ error: 'Already exist' });
     return;
   }
   // encrypt password and insert user in datbase
   const usr = { email, password: sha1(pwd) };
   const user = await dbClient.client.collection('users').insertOne(usr);
   if (user) res.status(201).json({ id: user.ops[0]._id, email: user.ops[0].email });
-  else res.status(500).end('Could not create user');
+  else res.status(500).json({ error: 'Could not create user' });
 }
 
 /** getMe - retrieves the user that is currently signed-in with its connection token
